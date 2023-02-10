cc18aa628b68da376bc64f268ec52f72aa908e60
wesley_wisch <wesley_wisch@hotmail.com>
2023-02-10 08:52:03 -0300

    feat: Starting the project and creating the structure.

---
+++ src/pages/Hotels/[hotel]/index.page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Hotel() {
+  return (
+    <div>
+      <h1>Hotel id</h1>
+    </div>
+  )
+}
---
+++ src/pages/Hotels/index.page.tsx
@@ -0,0 +1,8 @@
+
+export default function Hotels() {
+  return (
+    <div>
+      <h1>Hotels</h1>
+    </div>
+  )
+}
